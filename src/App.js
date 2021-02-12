a88d05d95f5199d21452f635431eb2207e82fb27
vrodgreg <75463166+vrodgreg@users.noreply.github.com>
2021-02-11 22:30:11 -0500

    Switch Button Code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Switch from "./Switch";
 
 function App() {
+  const [value, setValue] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Switch
+        isOn={value}
+        onColor="#FFA500"
+        handleToggle={() => setValue(!value)}
+      />
     </div>
   );
 }
